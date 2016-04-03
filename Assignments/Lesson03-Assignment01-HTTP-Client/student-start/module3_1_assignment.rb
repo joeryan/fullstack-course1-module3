d39880200b90ef055e8d0e06edff79e49f846a50
Joe Ryan <joe@clanryan.us>
2016-04-02 21:47:40 -0400

    complete practice programming assignment to make a simple api client

--- Assignments/Lesson03-Assignment01-HTTP-Client/student-start/module3_1_assignment.rb
+++ Assignments/Lesson03-Assignment01-HTTP-Client/student-start/module3_1_assignment.rb
@@ -1,5 +1,12 @@
 require 'httparty'
 
 class Recipe
+  include HTTParty
+  base_uri "http://food2fork.com/api"
+  default_params sort: 'r', key: ENV['FOOD2FORK_KEY']
+  format :json
 
+  def self.for term
+    get("/search", query: {query: term})["recipes"]
+  end
 end
